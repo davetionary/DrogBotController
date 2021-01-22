092b7fd2dbe442b47c5b5be108818c1bc00f9656
Dave Drogowski <davetionary@gmail.com>
2021-01-22 14:33:10 -0500

    Added motor control to button loop and red LED for "off" status

--- PibrellaController.py
+++ PibrellaController.py
@@ -1,11 +1,17 @@
 import pibrella
 import time
 
+# commented this out, not sure why I had it on
+# pibrella.output.g.on()
+
 var = 1
 
 while var == 1 :
 
-	time.sleep(1) 
+	time.sleep(.05) 
+
+	print 'Input Read'
+	print pibrella.input.a.read()
 
 	buttonstatus = pibrella.button.read() 
 
@@ -13,9 +19,13 @@ while var == 1 :
 
 		print 'Button State Is ', buttonstatus
 		pibrella.light.off()
+		pibrella.light.red.on()
+		pibrella.output.h.off()
 
 	if (buttonstatus == 1) :
    		print 'Button State Is ', buttonstatus
 		print 'GOTCHA'
+		pibrella.light.off()
 		pibrella.light.green.on()
+		pibrella.output.h.on()
 
